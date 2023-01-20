cc6918943bf78ac06dfc2a2c9dda6b24a404a38f
Mark C <ldcaorz@gmail.com>
2023-01-20 01:29:22 -0500

    small update after testing

--- src/main/java/com/cao/awss3/config/FileConfig.java
+++ src/main/java/com/cao/awss3/config/FileConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+
 @Configuration
 public class FileConfig {
 
